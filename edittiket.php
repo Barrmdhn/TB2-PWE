c36eeb248e047679b69bee2a464e8233b943e482
Barrmdhn <65026008+Barrmdhn@users.noreply.github.com>
2020-05-27 19:53:10 +0700

    Update edittiket.php

--- edittiket.php
+++ edittiket.php
@@ -6,7 +6,7 @@ include('template/navigasi.php');
  	session_start();
  }
  if (empty($_SESSION['username'])) {
- 	header('Location:login.php');
+ 	header('Location:index.html');
  }
 $id=$_GET['id_tiket'];
 
